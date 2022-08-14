485cd8d8c3ce150af371a5c969e7efd0a09515b7
M. Scott Ford <scott@corgibytes.com>
2022-08-14 00:37:09 -0400

    Introduces MavenRepository interface and default implementation
    This is a breaking change, so the major version number have been bumped as a result of this change.
    
    This is being done to make it easier to test certain scenarios where the target repository and Maven central may return different results. Tests for that scenario will come in further commits.

--- src/main/kotlin/com/corgibytes/maven/ReleaseHistoryService.kt
+++ src/main/kotlin/com/corgibytes/maven/ReleaseHistoryService.kt
@@ -1,150 +1,36 @@
 package com.corgibytes.maven
 
-import io.ktor.client.*
-import io.ktor.client.call.*
-import io.ktor.client.engine.cio.*
-import io.ktor.client.request.*
 import kotlinx.coroutines.async
 import kotlinx.coroutines.runBlocking
-import kotlinx.serialization.Serializable
-import nl.adaptivity.xmlutil.serialization.*
 import java.time.ZonedDateTime
-import java.time.format.DateTimeFormatter
 
-enum class Mode {
-    CONFIGURED_REPOSITORY,
-    MAVEN_CENTRAL
-}
+class ReleaseHistoryService(
+    private val targetRepository: MavenRepository,
+    private val mavenCentralRepository: MavenRepository) {
 
-class ReleaseHistoryService {
-    private val mavenCentralUrl = "https://repo.maven.apache.org/maven2"
-
-    var repositoryUrl: String = mavenCentralUrl
-    set(value) {
-        field = value.removeSuffix("/")
-    }
-
-    private val isUsingMavenCentral: Boolean
-        get() {
-            return repositoryUrl == mavenCentralUrl
-        }
-
-    private val client = HttpClient(CIO)
+    constructor(repositoryUrl: String = MavenRepositoryImpl.mavenCentralUrl) : this(
+        MavenRepositoryImpl(repositoryUrl),
+        MavenRepositoryImpl(MavenRepositoryImpl.mavenCentralUrl))
 
     fun getVersionHistory(groupId: String, artifactId: String): Map<String, ZonedDateTime> {
-        var mode = Mode.CONFIGURED_REPOSITORY
-        var versions = getVersionsFromMetadata(groupId, artifactId, mode)
+        var versions = targetRepository.getVersionsFromMetadata(groupId, artifactId)
 
-        if (versions.isEmpty() && !isUsingMavenCentral) {
-            mode = Mode.MAVEN_CENTRAL
-            versions = getVersionsFromMetadata(groupId, artifactId, mode)
+        var repositoryForVersions = targetRepository
+        if (versions.isEmpty() && !targetRepository.isMavenCentral) {
+            repositoryForVersions = mavenCentralRepository
+            versions = mavenCentralRepository.getVersionsFromMetadata(groupId, artifactId)
         }
 
         val result: Map<String, ZonedDateTime>
 
         runBlocking {
             result = versions.map { version ->
-                version to async { getVersionReleaseDate(groupId, artifactId, version, mode) }
+                version to async { repositoryForVersions.getVersionReleaseDate(groupId, artifactId, version) }
             }.associate { pair ->
                 pair.first to pair.second.await()
             }
         }
 
         return result
-    }
-
-    suspend fun getVersionReleaseDate(groupId: String, artifactId: String, version: String, mode: Mode = Mode.CONFIGURED_REPOSITORY): ZonedDateTime {
-        val url = buildVersionPomUrl(groupId, artifactId, version, mode)
-        val response = client.head(url)
-        var headerValue = response.headers["Last-Modified"]
-        if (headerValue == null) {
-            headerValue = response.headers["last-modified"]
-        }
-        if (headerValue == null) {
-            throw Exception("Last-Modified header is not present for $url")
-        }
-
-        return ZonedDateTime.parse(headerValue, DateTimeFormatter.RFC_1123_DATE_TIME)
-    }
-
-    @Serializable
-    private data class MetadataVersion(@XmlValue(true) val value: String)
-
-    @Serializable
-    private data class MetadataVersioning(
-        @XmlElement(true)
-        @XmlSerialName("latest", "", "")
-        val latest: String,
-        @XmlElement(true)
-        @XmlSerialName("release", "", "")
-        val release: String? = null,
-        @XmlElement(true)
-        @XmlSerialName("versions", "", "")
-        @XmlChildrenName("version", "", "")
-        val versions: List<MetadataVersion>,
-        @XmlElement(true)
-        @XmlSerialName("lastUpdated", "", "")
-        val lastUpdated: String
-    )
-
-    @Serializable
-    @XmlSerialName("metadata", "", "")
-    private data class Metadata(
-        @XmlElement(false)
-        @XmlSerialName("modelVersion", "", "")
-        val modelVersion: String? = null,
-        @XmlElement(true)
-        @XmlSerialName("groupId", "", "")
-        val groupId: String,
-        @XmlElement(true)
-        @XmlSerialName("artifactId", "", "")
-        val artifactId: String,
-        @XmlElement(true)
-        @XmlSerialName("version", "", "")
-        val version: String? = null,
-        @XmlElement(true)
-        @XmlSerialName("versioning", "", "")
-        val versioning: MetadataVersioning
-    )
-
-    fun getVersionsFromMetadata(groupId: String, artifactId: String, mode: Mode = Mode.CONFIGURED_REPOSITORY): List<String> {
-        val wasRequestSuccessful: Boolean
-        val responseBody: String
-        runBlocking {
-            val response = client.get(buildMetadataUrl(groupId, artifactId, mode))
-            wasRequestSuccessful = response.status.value < 400
-            responseBody = response.body()
-        }
-
-        return if (wasRequestSuccessful) {
-            parseVersionsFromMetadataResponse(responseBody)
-        } else {
-            emptyList()
-        }
-    }
-
-    fun buildMetadataUrl(groupId: String, artifactId: String, mode: Mode = Mode.CONFIGURED_REPOSITORY): String {
-        val targetRepositoryUrl = repositoryUrlFrom(mode)
-        return "$targetRepositoryUrl/${groupId.replace(".", "/")}/$artifactId/maven-metadata.xml"
-    }
-
-    fun buildVersionPomUrl(groupId: String, artifactId: String, version: String, mode: Mode = Mode.CONFIGURED_REPOSITORY): String {
-        val targetRepositoryUrl = repositoryUrlFrom(mode)
-        return "$targetRepositoryUrl/${groupId.replace(".", "/")}/$artifactId/$version/$artifactId-$version.pom"
-    }
-
-    fun parseVersionsFromMetadataResponse(responseBody: String): List<String> {
-        val metadata: Metadata = XML.decodeFromString(responseBody)
-        return metadata.versioning.versions.map { it.value }
-    }
-
-    private fun repositoryUrlFrom(mode: Mode) = when (mode) {
-        Mode.CONFIGURED_REPOSITORY -> {
-            repositoryUrl
-        }
-
-        Mode.MAVEN_CENTRAL -> {
-            mavenCentralUrl
-        }
     }
 }
--- src/test/kotlin/com/corgibytes/maven/ReleaseHistoryServiceTest.kt
+++ src/test/kotlin/com/corgibytes/maven/ReleaseHistoryServiceTest.kt
@@ -1,10 +1,8 @@
 package com.corgibytes.maven
 
-import kotlinx.coroutines.runBlocking
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 import kotlin.test.Test
-import kotlin.test.assertContains
 import kotlin.test.assertEquals
 
 class ReleaseHistoryServiceTest {
@@ -72,9 +70,7 @@ class ReleaseHistoryServiceTest {
 
     @Test
     fun retrieveReleaseHistoryFromAlternativeLocation() {
-        val service = ReleaseHistoryService()
-
-        service.repositoryUrl = "http://repo.spring.io/release"
+        val service = ReleaseHistoryService("http://repo.spring.io/release")
 
         val actualResults = service.getVersionHistory("org.springframework", "spring-core")
 
@@ -315,9 +311,7 @@ class ReleaseHistoryServiceTest {
 
     @Test
     fun retrieveReleaseHistoryFallsBackToMavenCentralIfPackageNotFoundInAlternativeRepository() {
-        val service = ReleaseHistoryService()
-
-        service.repositoryUrl = "http://repo.spring.io/release"
+        val service = ReleaseHistoryService("http://repo.spring.io/release")
 
         val actualResults = service.getVersionHistory("org.apache.ant", "ant")
 
@@ -366,192 +360,4 @@ class ReleaseHistoryServiceTest {
             assertEquals(expectedDateTime, actualResults.get(version), version)
         }
     }
-
-    @Test
-    fun retrievePackageVersions() {
-        val service = ReleaseHistoryService()
-
-        val actualResults = service.getVersionsFromMetadata("org.apache.maven", "apache-maven")
-
-        val expectedResults = listOf(
-            "2.0.9",
-            "2.0.10",
-            "2.0.11",
-            "2.1.0-M1",
-            "2.1.0",
-            "2.2.0",
-            "2.2.1",
-            "3.0-alpha-2",
-            "3.0-alpha-3",
-            "3.0-alpha-4",
-            "3.0-alpha-5",
-            "3.0-alpha-6",
-            "3.0-alpha-7",
-            "3.0-beta-1",
-            "3.0-beta-2",
-            "3.0-beta-3",
-            "3.0",
-            "3.0.1",
-            "3.0.2",
-            "3.0.3",
-            "3.0.4",
-            "3.0.5",
-            "3.1.0-alpha-1",
-            "3.1.0",
-            "3.1.1",
-            "3.2.1",
-            "3.2.2",
-            "3.2.3",
-            "3.2.5",
-            "3.3.1",
-            "3.3.3",
-            "3.3.9",
-            "3.5.0-alpha-1",
-            "3.5.0-beta-1",
-            "3.5.0",
-            "3.5.2",
-            "3.5.3",
-            "3.5.4",
-            "3.6.0",
-            "3.6.1",
-            "3.6.2",
-            "3.6.3",
-            "3.8.1",
-            "3.8.2",
-            "3.8.3",
-            "3.8.4",
-            "3.8.5",
-        )
-
-        expectedResults.forEach { assertContains(actualResults, it) }
-    }
-
-    @Test
-    fun buildMetadataUrlWithDefaultRepository() {
-        val service = ReleaseHistoryService()
-
-        assertEquals(
-            "https://repo.maven.apache.org/maven2/org/apache/maven/apache-maven/maven-metadata.xml",
-            service.buildMetadataUrl("org.apache.maven", "apache-maven")
-        )
-    }
-
-    @Test
-    fun buildMetadataUrlWithAlternativeRepository() {
-        val service = ReleaseHistoryService()
-
-        service.repositoryUrl = "https://repo.spring.io/artifactory/release/"
-
-        assertEquals(
-            "https://repo.spring.io/artifactory/release/io/spring/platform/platform/maven-metadata.xml",
-            service.buildMetadataUrl("io.spring.platform", "platform")
-        )
-    }
-
-    @Test
-    fun buildVersionPomUrl() {
-        val service = ReleaseHistoryService()
-
-        assertEquals(
-            "https://repo.maven.apache.org/maven2/org/apache/maven/apache-maven/2.0.10/apache-maven-2.0.10.pom",
-            service.buildVersionPomUrl("org.apache.maven", "apache-maven", "2.0.10")
-        )
-    }
-
-    @Test
-    fun getVersionReleaseDate() {
-        val service = ReleaseHistoryService()
-
-        val expectedDateTime =
-            ZonedDateTime.parse("Tue, 10 Feb 2009 02:57:59 GMT", DateTimeFormatter.RFC_1123_DATE_TIME)
-        runBlocking {
-            assertEquals(
-                expectedDateTime,
-                service.getVersionReleaseDate("org.apache.maven", "apache-maven", "2.0.10")
-            )
-        }
-    }
-
-    @Test
-    fun parseVersionsFromMetadataResponseWithOptionalFieldsMissing() {
-        // this XML document does not contain the /metadata/release element
-        val xmlDocument = """
-            <metadata>
-                <groupId>org.apache.maven</groupId>
-                <artifactId>maven-project</artifactId>
-                <versioning>
-                    <latest>2.2.1</latest>
-                    <versions>
-                        <version>2.0-alpha-2</version>
-                        <version>2.0-alpha-3</version>
-                        <version>2.0-beta-1</version>
-                        <version>2.0-beta-2</version>
-                        <version>2.0-beta-3</version>
-                        <version>2.0</version>
-                        <version>2.0-1</version>
-                        <version>2.0.1</version>
-                        <version>2.0.2</version>
-                        <version>2.0.3</version>
-                        <version>2.0.4</version>
-                        <version>2.0.5</version>
-                        <version>2.0.6</version>
-                        <version>2.0.7</version>
-                        <version>2.0.8</version>
-                        <version>2.0.9</version>
-                        <version>2.0.10</version>
-                        <version>2.0.11</version>
-                        <version>2.1.0-M1</version>
-                        <version>2.1.0</version>
-                        <version>2.2.0</version>
-                        <version>2.2.1</version>
-                        <version>3.0-alpha-1</version>
-                        <version>3.0-alpha-2</version>
-                    </versions>
-                    <lastUpdated>20100226050724</lastUpdated>
-                </versioning>
-            </metadata> 
-        """.trimIndent()
-
-        val service = ReleaseHistoryService()
-
-        val parsedVersions = service.parseVersionsFromMetadataResponse(xmlDocument)
-
-        val expectedResults = listOf(
-            "2.0-alpha-2",
-            "2.0-alpha-3",
-            "2.0-beta-1",
-            "2.0-beta-2",
-            "2.0-beta-3",
-            "2.0",
-            "2.0-1",
-            "2.0.1",
-            "2.0.2",
-            "2.0.3",
-            "2.0.4",
-            "2.0.5",
-            "2.0.6",
-            "2.0.7",
-            "2.0.8",
-            "2.0.9",
-            "2.0.10",
-            "2.0.11",
-            "2.1.0-M1",
-            "2.1.0",
-            "2.2.0",
-            "2.2.1",
-            "3.0-alpha-1",
-            "3.0-alpha-2",
-            "2.0.9",
-            "2.0.10",
-            "2.0.11",
-            "2.1.0-M1",
-            "2.1.0",
-            "2.2.0",
-            "2.2.1",
-        )
-
-        expectedResults.forEach { assertContains(parsedVersions, it) }
-
-    }
-
 }
